c4c6cef74e8d9ef82b933edc08380f917ae22b32
XSamurai <art.markovv@gmail.com>
2022-04-05 12:33:22 +0500

    Добавьте файлы проекта.

---
+++ MS-SQL Parser - Insert To Merge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS_SQL_Parser___Insert_To_Merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
