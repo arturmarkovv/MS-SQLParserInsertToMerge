dac02ad82a836e58a5b8cb5193025564d0ce9b3f
XSamurai <art.markovv@gmail.com>
2022-04-06 13:53:57 +0500

    Чистка комментов

--- MS-SQL Parser - Insert To Merge/Parser.cs
+++ MS-SQL Parser - Insert To Merge/Parser.cs
@@ -18,25 +18,23 @@ namespace MS_SQL_Parser___Insert_To_Merge
             string fieldPattern = @"(\[\w+\])(\,|\))";
             string valuesPattern = @"VALUES \((.*)\)";
 
-            var valuesStr = Regex.Match(inputData, valuesPattern).Groups[1].Value;
-            //string valuePattern = @"N\'.*?\'\,?|CAST\(N\'.*?\' AS DateTime2\)\,?|NULL\,?|\d+?\,?";
-            //string valuePattern = @"";
-
+            
             var nameMatch = Regex.Match(inputData, namePattern);
             var tableName = nameMatch.Groups[1].Value;
 
             var fieldMatches = Regex.Matches(inputData, fieldPattern);
-            var valueMatches = valuesStr.Split(", ");//Regex.Matches(valuesStr, valuePattern);
+
+            var valuesStr = Regex.Match(inputData, valuesPattern).Groups[1].Value;
+            var valueMatches = valuesStr.Split(", ");
 
             var fields = new List<string>();
             var values = new List<string>();
 
             for (int i = 0; i < fieldMatches.Count; i++)
             {
-                //Сдвиг из-за того, что имя таблицы тоже добавляется в поля
                 try
                 {
-                    fields.Add(fieldMatches[i].Groups[1].Value);
+                    fields.Add(fieldMatches[i].Groups[1].Value); //из группы без запятых
                     values.Add(valueMatches[i]);
                 }
                 catch 
